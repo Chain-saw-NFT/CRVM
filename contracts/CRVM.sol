4349ad3e573225b7abdc5ae4ea3eef0f1365866d
exp <exp.table@protonmail.com>
2021-12-20 20:02:50 +0100

    finished tests

--- contracts/CRVM.sol
+++ contracts/CRVM.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
 
 /// @title Christian Rex van Minnen's NFT drop
 /// @author exp.table
 contract CRVM is ERC721, Ownable {
+    using Strings for uint256;
     using BitMaps for BitMaps.BitMap;
 
     bool public isOpened;
     uint256 public constant PRICE = 0.08 ether;
-    uint256 public leftBlanks; // number of blanks left to be sold
 
+    uint256 private constant _SEPARATOR = 2000;
+    uint256 private _dropId;
     bytes32 private _merkleRoot;
+    string private _baseTokenURI = "ipfs://";
+    string private _ipfsCID;
     mapping (bytes32 => BitMaps.BitMap) private _claimed;
+    mapping (uint256 => uint256) private _leftBlanks;
 
     constructor() ERC721("CRVM", "CRVM") {}
 
@@ -29,42 +35,70 @@ contract CRVM is ERC721, Ownable {
         _merkleRoot = newMerkleRoot;
     }
 
-    /**
-        Mints all the 1/1s tokens at once, to be put on auction later.
-    */
-    function premint(address recipient) public onlyOwner {
-        for(uint256 i = 0; i < 50; i++) {
+    function setBaseURI(string calldata baseTokenURI) public onlyOwner {
+        _baseTokenURI = baseTokenURI;
+    }
+
+    function updateCID(string calldata newCID) public onlyOwner {
+        _ipfsCID = newCID;
+    }
+
+    function flipOpen() public onlyOwner {
+        isOpened = !isOpened;
+    }
+
+    /// @notice Mints "core" tokens, 10 per drop, until all 5 drops are done
+    function coreMint(address recipient, string calldata newCID, bytes32 newMerkleRoot) public onlyOwner {
+        uint256 dropId = _dropId++;
+        require(dropId < 5);
+        _leftBlanks[dropId] = 2000;
+        isOpened = false;
+        _ipfsCID = newCID;
+        _merkleRoot = newMerkleRoot;
+        for(uint256 i = 10*dropId; i < 10*dropId + 10; i++) {
             _safeMint(recipient, i);
         }
     }
 
-    function releaseBlanks() public onlyOwner {
-        leftBlanks += 2000;
-        isOpened = false;
+    function _internalMint(uint256 dropId, address recipient, uint256 quantity) internal {
+        uint256 start = (dropId+1) * _SEPARATOR + (_SEPARATOR - _leftBlanks[dropId]);
+        for(uint256 i = 0; i < quantity; i++) {
+            _safeMint(recipient, start+i);
+        }
+        _leftBlanks[dropId] -= quantity;
     }
 
-    function premint(uint256 quantity, uint256 index, bytes32[] calldata proof) public payable {
+    /// @notice Merkle tree members can buy before everyone else
+    /// @dev Users can buy it as soon as data is available, not according to isOpened
+    function merkleMint(uint256 dropId, uint256 quantity, uint256 index, bytes32[] calldata proof) public payable {
         require(!_claimed[_merkleRoot].get(index),"Claimed already");
         require(quantity <= 10, "Limit of 10 per tx");
         bytes32 node = keccak256(abi.encodePacked(msg.sender, index));
-        require(MerkleProof.verify(proof, _merkleRoot, leaf), "Invalid proof");
+        require(MerkleProof.verify(proof, _merkleRoot, node), "Invalid proof");
         _claimed[_merkleRoot].set(index);
-        leftBlanks -= quantity; //will revert if not enough blanks left
-        for(uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, i);
-        }
+        _internalMint(dropId, msg.sender, quantity);
     }
 
-    function mintBlank(uint256 quantity) public payable {
+    /// @notice Public minting of the blanks
+    function publicMint(uint256 dropId, uint256 quantity) public payable {
         require(isOpened, "Closed");
         require(quantity * PRICE == msg.value, "Incorrect eth amount");
         require(quantity <= 10, "Limit of 10 per tx");
-        leftBlanks -= quantity; //will revert if not enough blanks left
-        for(uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, i);
+        _internalMint(dropId, msg.sender, quantity);
+    }
+
+    function tokenURI(uint256 tokenId) public override view returns (string memory) {
+        if (tokenId < 50) {
+            return string(abi.encodePacked(_baseTokenURI, _ipfsCID, "/", tokenId.toString()));
+        } else {
+            uint256 blankId = (tokenId / _SEPARATOR) * _SEPARATOR; // produce 2000 | 4000 | ... | 10000
+            return string(abi.encodePacked(_baseTokenURI, _ipfsCID, "/", blankId.toString()));
         }
     }
 
-
+    function burn(uint256 tokenId) public virtual {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "caller is not owner nor approved");
+        _burn(tokenId);
+    }
 
 }
---
+++ test/test.js
@@ -0,0 +1,116 @@
+const { expect } = require("chai");
+const { waffle } = require("hardhat");
+const provider = waffle.provider;
+const whitelist1 = require('./whitelist1.json');
+const whitelist2 = require('./whitelist2.json');
+
+
+function getPrice(price) {
+  return (price * (10**18)).toString();
+}
+
+describe("CRVM", function () {
+  let owner, client1, client2, crvm, crvm1, crvm2;
+
+  const merkleRoot1 = "0xf704c969678468a7533f5fb7516de020fb9073aa0cdd921ff66a200d0a0f6798";
+  const merkleRoot2 = "0x916877b3e7c7f9b6a17e44385ae52f8cb7c7c25ce01f1cc1f0f67ae71d555388";
+
+  before(async function () {
+    [owner, client1, client2] = await ethers.getSigners();
+
+    const CRVM = await ethers.getContractFactory("CRVM");
+    crvm = await CRVM.deploy();
+    await crvm.deployed();
+
+    crvm1 = await crvm.connect(client1);
+    crvm2 = await crvm.connect(client2);
+  });
+
+  it("Drop #0 - Can't buy in public sale when closed", async function () {
+    await expect(crvm1.publicMint(0, 2, {value:getPrice(2*0.08)})).to.be.revertedWith("Closed");
+  });
+
+  it("Drop #0 - Owner calls coreMint", async function() {
+    await crvm.coreMint(owner.address, "a", merkleRoot1);
+    //10 tokens should have been minted
+    expect(await crvm.balanceOf(owner.address)).to.equal(10);
+    //tokenURI test
+    expect(await crvm.tokenURI(9)).to.equal("ipfs://a/9");
+  });
+
+  it("Drop #0 - Can't buy into public sale with wrong price", async function() {
+    await crvm.flipOpen();
+    await expect(crvm1.publicMint(0, 2, {value:getPrice(0.08)})).to.be.revertedWith("Incorrect eth amount");
+  });
+
+  it("Drop #0 - Can buy into public sale", async function() {
+    await crvm1.publicMint(0, 2, {value:getPrice(2*0.08)});
+    //check balance
+    expect(await crvm.balanceOf(client1.address)).to.equal(2);
+    //check tokenURI
+    expect(await crvm.tokenURI(2000)).to.equal("ipfs://a/2000");
+    expect(await crvm.tokenURI(2001)).to.equal("ipfs://a/2000");
+  });
+
+  it("Drop #0 - Can buy into merkle mint", async function() {
+    const data = whitelist1[client1.address.toLowerCase()];
+    await crvm1.merkleMint(0, 8, data.index, data.proof, {value:getPrice(8*0.08)});
+    //check balance
+    expect(await crvm.balanceOf(client1.address)).to.equal(10);
+  });
+
+  it("Drop #0 - Cannot rebuy into merkle mint", async function() {
+    const data = whitelist1[client1.address.toLowerCase()];
+    await expect(crvm1.merkleMint(0, 8, data.index, data.proof, {value:getPrice(8*0.08)})).to.be.revertedWith("Claimed already");
+  });
+
+  it("Drop #0 - Blanks sale frop drop 0 sold out", async function() {
+    for(let i = 0 ; i < 199; i++) await crvm1.publicMint(0, 10, {value:getPrice(10*0.08)});
+    await expect(crvm1.publicMint(0, 10, {value:getPrice(10*0.08)})).to.be.reverted;
+  });
+
+  it("Drop #1 - core mint", async function() {
+    await crvm.coreMint(owner.address, "b", merkleRoot2);
+    //10 tokens should have been minted + 10 from before
+    expect(await crvm.balanceOf(owner.address)).to.equal(20);
+    //tokenURI test of previous batch
+    expect(await crvm.tokenURI(9)).to.equal("ipfs://b/9");
+    //tokenURI test of new batch
+    expect(await crvm.tokenURI(15)).to.equal("ipfs://b/15");
+    expect(await crvm.isOpened()).to.equal(false);
+  });
+
+  it("Drop #1 - cannot use previous merkle proof to mint", async function() {
+    const data = whitelist1[client1.address.toLowerCase()];
+    await expect(crvm1.merkleMint(1, 8, data.index, data.proof, {value:getPrice(8*0.08)})).to.be.revertedWith("Invalid proof");
+  });
+
+  it("Drop #1 - public mint + tokenURI", async function() {
+    await crvm.flipOpen();
+    await crvm2.publicMint(1, 2, {value:getPrice(2*0.08)});
+    //check balance
+    expect(await crvm.balanceOf(client2.address)).to.equal(2);
+    //check tokenURI
+    expect(await crvm.tokenURI(4000)).to.equal("ipfs://b/4000");
+    expect(await crvm.tokenURI(4001)).to.equal("ipfs://b/4000");
+  });
+
+  it("Owner cannot create more than 5 drops", async function() {
+    for(let i = 0; i < 3; i++) await crvm.coreMint(owner.address, "c", merkleRoot2);
+    await expect(crvm.coreMint(owner.address, "c", merkleRoot2)).to.be.reverted;
+  });
+
+  it("Withdraw eth", async function() {
+    const preBalance = await provider.getBalance(crvm.address);
+    await crvm.withdraw("0xd2927a91570146218ed700566df516d67c5ecfab");
+    expect(await provider.getBalance("0xd2927a91570146218ed700566df516d67c5ecfab")).to.be.equal(preBalance.toString());
+  });
+
+  it("Can burn token", async function() {
+    await expect(crvm.burn(4000)).to.be.reverted;
+    await crvm2.burn(4000);
+    expect(await crvm.balanceOf(client2.address)).to.equal(1);
+  });
+
+
+});
